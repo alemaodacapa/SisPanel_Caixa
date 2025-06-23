230aaf3d743129109f842770068c8a3d31420857
Leonardo Frederico de Paula <summerside@escoladotrabalhador40.com.br>
2025-06-23 14:19:11 -0300

    Update registro.php

--- registro.php
+++ registro.php
@@ -59,7 +59,7 @@ include 'conexao.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SisPainel Consultório - Operador</title>
+    <title>SisPainel Caixa - Operador</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
