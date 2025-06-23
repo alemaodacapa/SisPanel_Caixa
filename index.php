bd02af00163cf553061b5761b6614ffb9c276c96
Leonardo Frederico de Paula <summerside@escoladotrabalhador40.com.br>
2025-06-23 16:29:03 -0300

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['senha'])) {
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <meta property="og:image" content="/img/Banner.jpg">
+    <meta property="og:image" content="/img/caixa.jpg">
     <title>SisPainel - Acesso</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet"/>
